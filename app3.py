0ec9b731d5233a0b9ed8a87f9133a8912ddafa6f
yoosoeun0704 <yoosoeun0704@gmail.com>
2024-11-08 13:43:01 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -431,7 +431,7 @@ if st.button("추천받기"):
     matching_scores = []
 
     for destination in destinations:
-        # 각 관광지의 태그와 사용자 답변의 교집합 수를 세기
+        # 각 관광지의 태그와 사용자 답변의 일치 태그 수를 세기
         score = sum(1 for tag in destination["tags"] if tag in st.session_state.user_answers)
         matching_scores.append((destination, score))
     
@@ -440,9 +440,9 @@ if st.button("추천받기"):
     top_destinations = [destination for destination, score in matching_scores[:4]]
 
     # 사용자가 선택한 태그 중 우선순위 태그와 일치하는 관광지 필터링
-    priority_tags = ["문화", "역사 탐방", "자연 탐험", "쇼핑", "액티비티", 
+    priority_tags = ["문화/역사 탐방", "자연 탐험", "쇼핑", "액티비티", 
                      "문학적 활동", "음악 활동", "무용 활동", "미술 활동", 
-                     "도심", "자연", "바다", "유적지"]
+                     "도심", "자연", "유적지"]
     
     priority_destinations = [
         destination for destination in top_destinations
